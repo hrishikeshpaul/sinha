fb5de2336007d88fd82b939d9f4db47dea54e289
hrishikeshpaul <hrishikeshpaul12@gmail.com>
2023-06-19 16:40:12 -0700

    added about me section

--- src/sections/about-me/AboutMe.tsx
+++ src/sections/about-me/AboutMe.tsx
@@ -1,4 +1,6 @@
 import { FC } from "react";
+import { Flex, Image, Stack, Text } from "@chakra-ui/react";
+
 import { Section } from "sections/Section";
 import { SectionIds } from "util/constants/Sections";
 
@@ -9,7 +11,35 @@ export const AboutMe: FC = () => {
             className="section-about-me"
             title="ABOUT ME"
         >
-            hi
+            <Stack gap="16">
+                <Flex
+                    gap="8"
+                    alignItems="center"
+                    flexDir={{ base: "column", md: "row" }}
+                    position="relative"
+                >
+                    <Image
+                        width={{ base: "100%", md: "50%" }}
+                        src="about-me-1.jpeg"
+                    />
+                    <Text
+                        fontSize="lg"
+                        p={{ base: "0", md: 4 }}
+                        bg="gray.900"
+                        position={{ base: "relative", md: "absolute" }}
+                        width={{ base: "100%", md: "50%" }}
+                        right={{ base: "0", md: 8 }}
+                    >
+                        It was a good idea. At least, they all thought it was a
+                        good idea at the time. Hindsight would reveal that in
+                        reality, it was an unbelievably terrible idea, but it
+                        would take another week for them to understand that.
+                        Right now, at this very moment. they all agreed that it
+                        was the perfect course of action for the current
+                        situation.
+                    </Text>
+                </Flex>
+            </Stack>
         </Section>
     );
 };
